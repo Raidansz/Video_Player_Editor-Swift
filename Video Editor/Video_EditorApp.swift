48bab7bfd4cfc3eb4171312cde76729787070d2b
Raidan <92235772+Raidansz@users.noreply.github.com>
2025-03-01 22:07:21 +0100

    Initial Commit

---
+++ Video Editor/Video_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_EditorApp.swift
+//  Video Editor
+//
+//  Created by Raidan on 2025. 03. 01..
+//
+
+import SwiftUI
+
+@main
+struct Video_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Video EditorTests/Video_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_EditorTests.swift
+//  Video EditorTests
+//
+//  Created by Raidan on 2025. 03. 01..
+//
+
+import Testing
+@testable import Video_Editor
+
+struct Video_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
